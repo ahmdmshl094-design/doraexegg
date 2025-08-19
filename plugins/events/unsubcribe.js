2b7f79097d18b46f9b54f796ac6432cfdc59ffe9
rakomoro <yisfmohamm4466@gmail.com>
2025-08-19 16:34:32 -0700

    Add files via upload

--- plugins/events/unsubcribe.js
+++ plugins/events/unsubcribe.js
@@ -1,96 +1,148 @@
-// import moment from 'moment-timezone';
+import { createCanvas, loadImage} from "canvas";
+import axios from "axios";
+import fs from "fs-extra";
+import path from "path";
 
-// const logger = text => global.modules.get("logger").custom(text, moment().tz(global.config.timezone).format('YYYY-MM-DD_HH:mm:ss'));
+const backgrounds = [
+  "https://i.imgur.com/dDSh0wc.jpeg",
+  "https://i.imgur.com/UucSRWJ.jpeg",
+  "https://i.imgur.com/OYzHKNE.jpeg",
+  "https://i.imgur.com/V5L9dPi.jpeg",
+  "https://i.imgur.com/M7HEAMA.jpeg"
+];
 
-export default async function ({ event }) {
-    const { api } = global;
-    const { threadID, author, logMessageData } = event;
-    const { Threads, Users } = global.controllers;
-    const getThread = await Threads.get(threadID) || {};
+async function getAvatarUrl(userID) {
+  try {
+    const res = await axios.post("https://www.facebook.com/api/graphql/", null, {
+      params: {
+        doc_id: "5341536295888250",
+        variables: JSON.stringify({
+          height: 400,
+          scale: 1,
+          userID,
+          width: 400
+})
+}
+});
+    return res.data.data.profile.profile_picture.uri;
+} catch {
+    return "https://i.ibb.co/bBSpr5v/143086968-2856368904622192-1959732218791162458-n.png";
+}
+}
 
-    const getThreadInfo = getThread.info || {};
+async function createLeaveCard({ userID, username, threadName, threadID}) {
+  const canvas = createCanvas(800, 400);
+  const ctx = canvas.getContext("2d");
 
+  const avatarUrl = await getAvatarUrl(userID);
+  const backgroundUrl = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 
-    if (Object.keys(getThreadInfo).length === 0) return;
-    // getThreadInfo.members.splice(getThreadInfo.members.findIndex(mem => mem.userID == String(logMessageData.leftParticipantFbId)), 1);
-    const leftMemberIndex = getThreadInfo.members.findIndex(mem => mem.userID == logMessageData.leftParticipantFbId);
-    if (leftMemberIndex > -1) {
-        delete getThreadInfo.members[leftMemberIndex].exp;
-    }
+  const background = await loadImage(backgroundUrl);
+  const avatar = await loadImage(avatarUrl);
 
-    const type = (author == logMessageData.leftParticipantFbId) ? "left" : "kicked";
-    const authorName = (await Users.getInfo(author))?.name || author;
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    if (logMessageData.leftParticipantFbId == botID) {
-        // logger(`${threadID} • ${author} removed bot from thread`);
-        getThreadInfo.isSubscribed = false;
+  const avatarSize = 150;
+  const avatarX = 50;
+  const avatarY = canvas.height / 2 - avatarSize / 2;
 
-        let atlertMsg = getLang(`plugins.events.unsubcribe.bot.${type}`, {
-            authorName: authorName,
-            authorId: author,
-            threadName: getThreadInfo.name,
-            threadId: threadID
-        });
-        for (const adid of global.config.MODERATORS) {
-            global.sleep(300);
-            if (adid != threadID) {
-                api.sendMessage(atlertMsg, adid);
-            }
-        }
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(avatarX + avatarSize / 2, avatarY + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
+  ctx.drawImage(avatar, avatarX, avatarY, avatarSize, avatarSize);
+  ctx.restore();
 
-        return;
-    } else if (getThread.data?.notifyChange?.status === true) {
-        // const leftName = (await Users.getInfo(logMessageData.leftParticipantFbId))?.name || logMessageData.leftParticipantFbId;
+  ctx.fillStyle = "#ffffff";
+  ctx.font = "bold 32px Sans";
+  ctx.fillText(`${username} غادر المجموعة`, 230, 150);
+  ctx.fillText(`من ${threadName}`, 230, 200);
+  ctx.fillText(`نتمنى له التوفيق!`, 230, 250);
 
-        // let atlertMsg = getLang(`plugins.events.unsubcribe.${type}`, {
-        //     authorName: authorName,
-        //     authorId: author,
-        //     leftName: leftName,
-        //     leftId: logMessageData.leftParticipantFbId
-        // });
+  const outputPath = path.join(global.mainPath, "plugins/events/unsubcribeGifs", `${threadID}.png`);
+  await fs.ensureDir(path.dirname(outputPath));
+  const buffer = canvas.toBuffer("image/png");
+  fs.writeFileSync(outputPath, buffer);
 
-        // for (const rUID of getThread.data.notifyChange.registered) {
-        //     global.sleep(300);
-        //     api.sendMessage(atlertMsg, rUID);
-        // }
-    };
+  return fs.createReadStream(outputPath);
+}
 
-    let callback = async () => {
-        const leftName = (await Users.getInfo(logMessageData.leftParticipantFbId))?.name || logMessageData.leftParticipantFbId;
+export default async function ({ event}) {
+  const { api} = global;
+  const { threadID, author, logMessageData} = event;
+  const { Threads, Users} = global.controllers;
+  const getThread = await Threads.get(threadID) || {};
+  const getThreadInfo = getThread.info || {};
 
-        let atlertMsg = {
-            body: (getThread?.data?.leaveMessage ?
-                getThread.data.leaveMessage : getLang(`plugins.events.unsubcribe.${type}`))
-                .replace(/\{leftName}/g, leftName),
-            mentions: [{
-                tag: leftName,
-                id: logMessageData.leftParticipantFbId
-            }]
-        }
+  if (Object.keys(getThreadInfo).length === 0) return;
 
-        const gifPath = `${global.mainPath}/plugins/events/unsubcribeGifs/${threadID}.gif`;
-        if (global.isExists(gifPath)) {
-            atlertMsg.attachment = [await global.getStream(gifPath)];
-        }
+  const leftMemberIndex = getThreadInfo.members.findIndex(mem => mem.userID == logMessageData.leftParticipantFbId);
+  if (leftMemberIndex> -1) {
+    delete getThreadInfo.members[leftMemberIndex].exp;
+}
 
-        api.sendMessage(atlertMsg, threadID);
-    }
+  const type = (author == logMessageData.leftParticipantFbId)? "left": "kicked";
+  const authorName = (await Users.getInfo(author))?.name || author;
 
-    if (getThread?.data?.antiSettings?.antiOut && type == "left") {
-        global.api.addUserToGroup(logMessageData.leftParticipantFbId, threadID, async (err) => {
-            let needNotify = getThread?.data?.antiSettings?.notifyChange === true;
-            if (err) {
-                await callback();
+  if (logMessageData.leftParticipantFbId == botID) {
+    getThreadInfo.isSubscribed = false;
 
-                console.error(err);
-                if (needNotify) global.api.sendMessage(getLang("plugins.events.unsubcribe.antiOut.error"), threadID);
-            } else {
-                if (needNotify) global.api.sendMessage(getLang("plugins.events.unsubcribe.antiOut.success"), threadID);
-            }
-        })
-    } else await callback();
+    let atlertMsg = getLang(`plugins.events.unsubcribe.bot.${type}`, {
+      authorName,
+      authorId: author,
+      threadName: getThreadInfo.name,
+      threadId: threadID
+});
 
-    await Threads.updateInfo(threadID, { members: getThreadInfo.members, isSubscribed: getThreadInfo.isSubscribed });
+    for (const adid of global.config.MODERATORS) {
+      global.sleep(300);
+      if (adid!= threadID) {
+        api.sendMessage(atlertMsg, adid);
+}
+}
 
     return;
+}
+
+  const leftName = (await Users.getInfo(logMessageData.leftParticipantFbId))?.name || logMessageData.leftParticipantFbId;
+
+  let atlertMsg = {
+    body: (getThread?.data?.leaveMessage || getLang(`plugins.events.unsubcribe.${type}`))
+.replace(/\{leftName}/g, leftName),
+      mentions: [{
+      tag: leftName,
+      id: logMessageData.leftParticipantFbId
+}]
+};
+
+  const leaveCard = await createLeaveCard({
+    userID: logMessageData.leftParticipantFbId,
+    username: leftName,
+    threadName: getThreadInfo.name || threadID,
+    threadID
+});
+
+  atlertMsg.attachment = [leaveCard];
+
+  if (getThread?.data?.antiSettings?.antiOut && type === "left") {
+    global.api.addUserToGroup(logMessageData.leftParticipantFbId, threadID, async (err) => {
+      let needNotify = getThread?.data?.antiSettings?.notifyChange === true;
+      if (err) {
+        await api.sendMessage(atlertMsg, threadID);
+        if (needNotify) global.api.sendMessage(getLang("plugins.events.unsubcribe.antiOut.error"), threadID);
+} else {
+        if (needNotify) global.api.sendMessage(getLang("plugins.events.unsubcribe.antiOut.success"), threadID);
+}
+});
+} else {
+    await api.sendMessage(atlertMsg, threadID);
+}
+
+  await Threads.updateInfo(threadID, {
+    members: getThreadInfo.members,
+    isSubscribed: getThreadInfo.isSubscribed
+});
+
+  return;
 }
